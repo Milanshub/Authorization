4c3bf0ed5ea49a7626fb36e9aeb560297fcc44b7
Milan <m26030820@gmail.com>
2023-12-19 19:55:35 +0100

    google oauth process

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ import mongoose from "mongoose";
 import session from "express-session";
 import passport from 'passport';
 import passportLocalMongoose from "passport-local-mongoose"
+import GoogleStrategy from 'passport-google-oauth20';
+import findOrCreate from "mongoose-findorcreate";
 
 // const saltRounds = 10;
 const app = express();
@@ -30,7 +32,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
+//HELLO
 
 // connects to local mondoDB database and creates userDB 
 //, {useNewUrlParser: true} blabla
@@ -43,12 +45,14 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema ({
     email: String, 
     password: String,
-
+    googleId: String,
+    secret: String
 });
 
 
 // passport connects to MongoDB 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // create a constant 'secret', which is going to be th key and add encrypt package 
 // as a plugin. Encryption only 'password'
@@ -62,10 +66,30 @@ const User = new mongoose.model("User", userSchema);
 // passport is used on the user 
 passport.use(User.createStrategy());
 
-// passport saves the information of the user and then releases it
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
 
+passport.deserializeUser(function (id, done){
+    User.findById(id, function (err, user){
+        done(err,user);
+    })
+})
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 // We use express to get the home page from the server 
@@ -73,6 +97,18 @@ app.get("/", function (reg, res) {
         res.render("home");
     });
 
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/secrets");
+  });
 
 // We use express to get the login from the server 
 app.get("/login", function (reg, res) {
@@ -85,22 +121,58 @@ app.get("/register", function (reg, res){
         res.render("register");
     });
 
-// check if the user is authenticated
-app.get("/secrets", function (req,res){
-    if (req.isAuthenticated()){
-        res.render("secrets");
-    } else { 
-        res.redirect("/login");
-    }
-});
 
-app.get("/logout", function (req, res, next){
-    req.logout(function(err) {
-        if (err) { return next(err); }
-    res.redirect("/");
-    });
-});
+app.get("/secrets", function(req, res){
+        User.find({"secret": {$ne: null}}, function(err, foundUsers){
+          if (err){
+            console.log(err);
+          } else {
+            if (foundUsers) {
+              res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+          }
+        });
+      });
 
+      app.get("/submit", function(req, res){
+        if (req.isAuthenticated()){
+          res.render("submit");
+        } else {
+          res.redirect("/login");
+        }
+      });
+      
+app.get("/submit", function(req, res){
+        if (req.isAuthenticated()){
+          res.render("submit");
+        } else {
+          res.redirect("/login");
+        }
+      });
+      app.post("/submit", function(req, res){
+        const submittedSecret = req.body.secret;
+      
+      //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+        // console.log(req.user.id);
+      
+        User.findById(req.user.id, function(err, foundUser){
+          if (err) {
+            console.log(err);
+          } else {
+            if (foundUser) {
+              foundUser.secret = submittedSecret;
+              foundUser.save(function(){
+                res.redirect("/secrets");
+              });
+            }
+          }
+        });
+      });
+
+ app.get("/logout", function(req, res){
+        req.logout();
+        res.redirect("/");
+      });         
 // when the user in '/register' he can do a POST request, that..
 // we can catch, save in the DB and then open the 'secrets render' 
 app.post("/register", function (req, res){
@@ -136,6 +208,8 @@ app.post("/login", function (req, res){
 });
 
 
+
 app.listen(3000, function() {
     console.log("The server has started on port 3000")
 })
+
